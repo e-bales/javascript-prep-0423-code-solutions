dc581b212869bd0e23d3ac89c801c1b84b0e2e6a
Eli Bales <elibales21@gmail.com>
2023-05-06 17:44:24 +0000

    completed javascript function exercises

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,45 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+const var1 = addTwoNumbers(10, 11);
+console.log(var1);
+function converHoursToMinutes(hours) {
+  return hours * 60;
+}
+const var2 = converHoursToMinutes(10);
+console.log(var2);
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+const var3 = getGreeting('Cotton-eyed Joe');
+console.log(var3);
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+const var4 = addAndMultiplyBy5(10, 4);
+console.log(var4);
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+const var5 = multiplyAndDivideBy5(10, 2);
+console.log(var5);
+function subTractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+const var6 = subTractTwoNumbers(10, 3);
+console.log(var6);
+function getCircleCircumference(radius) {
+  return radius * 2 * Math.PI;
+}
+const var7 = getCircleCircumference(5);
+console.log(var7);
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+const var8 = getFullName('George', 'Clooney');
+console.log(var8);
+function cube(number) {
+  return number * number * number;
+}
+const var9 = cube(10);
+console.log(var9);
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,40 @@
+# prep-javascript-functions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are the five parts of a function definition?
+  the function keyword, the optional name, the lsit of parameters, the actual codeblock, and an optional return statement
+- How do you call a function?
+  after actually defining the function, you have to call it by typing the function name followed by parentheses. For example, if you have a function called logOutput, you would call it with logOutput().
+- What is the difference between a parameter and an argument?
+  the parameter is the placeholder name of what is being passed in, while the argument is what is actually passed in.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
